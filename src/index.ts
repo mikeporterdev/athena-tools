c4a35c51f00ef43e7ea939bd230dfb7558f226a0
Mike Porter <mike@graphyapp.com>
2024-04-21 12:08:55 +0100

    fix issue with spaces in paths and options

--- src/index.ts
+++ src/index.ts
@@ -15,22 +15,23 @@ program
     .option('--hostName <hostName>', 'The IP address or hostname of the printer, do not include http')
     .action(async (pathToLycheeNanoDlp, options) => {
 
-        console.log(`Running fixer on ${pathToLycheeNanoDlp}`);
+      const cleanedNanodlpPath = pathToLycheeNanoDlp.trim();
+      console.log(`Running fixer on ${cleanedNanodlpPath}`);
 
         const { profileId, hostName } = options;
 
-        const selectedHostName = hostName ? hostName : await input({ message: 'Enter your printer IP address, eg. enterprising-senna.local or 192.168.0.1, (do not include http): '})
+        const selectedHostName = hostName?.trim() ?? await input({message: 'Enter your printer IP address, eg. enterprising-senna.local or 192.168.0.1, (do not include http): '})
 
         const profiles = await getPrinterProfiles(selectedHostName);
 
-        const selectedProfileId = profileId ? profileId : await select({
-            message: 'Please choose a profile:',
-            choices: profiles.map(profile => {
-                return {
-                    name: profile.Title,
-                    value: profile.ProfileID
-                }
-            })
+        const selectedProfileId = profileId?.trim() ?? await select({
+          message: 'Please choose a profile:',
+          choices: profiles.map(profile => {
+            return {
+              name: profile.Title,
+              value: profile.ProfileID
+            }
+          })
         })
 
         const selectedProfile = profiles.find(profile => profile.ProfileID == selectedProfileId);
@@ -41,11 +42,11 @@ program
 
         console.log(`Selected profile ${selectedProfile.Title}`)
 
-        console.log(`Editing nanodlp ${pathToLycheeNanoDlp}`)
-        await updateFile(pathToLycheeNanoDlp, selectedProfile)
+        console.log(`Editing nanodlp ${cleanedNanodlpPath}`)
+        await updateFile(cleanedNanodlpPath, selectedProfile)
         console.log(`NanoDLP successfully cleaned and ready for upload`)
         console.log('Uploading file')
-        await uploadCleanedFile(selectedHostName, pathToLycheeNanoDlp, selectedProfile.ProfileID)
+        await uploadCleanedFile(selectedHostName, cleanedNanodlpPath, selectedProfile.ProfileID)
     });
 
 program.parse(process.argv)
